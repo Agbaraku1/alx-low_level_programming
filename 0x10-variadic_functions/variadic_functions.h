07e82427281e58cf852131cf0c8ab6c90ee3290e
Marcus <Agbarakum@gmail.com>
2023-03-24 12:20:16 -0700

    add all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,14 +9,10 @@ int print_c(va_list a);
 int print_i(va_list a);
 int print_f(va_list a);
 int print_s(va_list a);
-/**
- * struct op - struct for 3
- * @c: character to check
- * @f: function to point to
- */
 typedef struct op
 {
 	char *c;
 	int (*f)(va_list a);
 } printer;
 #endif
+
